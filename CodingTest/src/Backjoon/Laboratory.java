01c66927681da04269d2a18e53cfc8a0dc40080b
Yoon321 <tkdwls734@gmail.com>
2024-01-23 00:10:53 +0900

    [Backjoon-14502] 연구소
    https://www.acmicpc.net/problem/14502

--- CodingTest/src/Backjoon/Laboratory.java
+++ CodingTest/src/Backjoon/Laboratory.java
@@ -36,49 +36,46 @@ public class Laboratory {
 
     public static void bruteForce(int start, int k, int[][] map, List<int[]> emptyList, List<Integer> cur){
         int length = emptyList.size();
-        int count = 0;
         if (cur.size() == k) {
             int curEmptyArea = 0;
-
-            Map<int[], Boolean> wallMap = new HashMap<>();
+            int count = 0;
+            Map<Point, Boolean> wallMap = new HashMap<>();
             for (int idx : cur) {
                 int[] wall = emptyList.get(idx);
-                wallMap.put(wall, true);
+                wallMap.put(new Point(wall[0], wall[1]), true);
             }
 
             for (int i = 0; i < map.length; i++) {
                 for (int j = 0; j < map[0].length; j++) {
                     if (map[i][j] == 1) {
-                        wallMap.put(new int[]{i, j}, true);
+                        wallMap.put(new Point(i, j), true);
                     }
                 }
             }
-
             // bfs 코드
-            Queue<int[]> queue = new LinkedList<>();
-            Map<int[], Boolean> visited = new HashMap<>();
+            Queue<Point> queue = new LinkedList<>();
+            Map<Point, Boolean> visited = new HashMap<>();
 
             for (int i = 0; i < map.length; i++) {
                 for (int j = 0; j < map[0].length; j++) {
                     if (map[i][j] == 2) {
-                        queue.offer(new int[]{i, j});
-                        visited.put(new int[]{i, j}, true);
+                        queue.offer(new Point(i, j));
+                        visited.put(new Point(i, j), true);
                         count++;
                     }
                 }
             }
 
             while(!queue.isEmpty()){
-                int[] now = queue.poll();
-
+                Point now = queue.poll();
                 for (int[] move : moves) {
-                    int nextR = now[0] + move[0];
-                    int nextC = now[1] + move[1];
-
+                    int nextR = now.row + move[0];
+                    int nextC = now.col + move[1];
                     if (nextR >= 0 && nextR < map.length && nextC >= 0 && nextC < map[0].length) {
-                        if (!visited.containsKey(new int[]{nextR, nextC}) && !wallMap.containsKey(new int[]{nextR, nextC})) {
-                            queue.offer(new int[]{nextR, nextC});
-                            visited.put(new int[]{nextR, nextC}, true);
+
+                        if (!visited.containsKey(new Point(nextR, nextC)) && !wallMap.containsKey(new Point(nextR, nextC))) {
+                            queue.offer(new Point(nextR, nextC));
+                            visited.put(new Point(nextR, nextC), true);
                             count++;
                         }
                     }
@@ -90,10 +87,43 @@ public class Laboratory {
         }
 
         for (int i = start; i < length; i++) {
-            cur.add(i);
-            bruteForce(start + 1, k, map, emptyList, cur);
-            cur.remove(cur.size() - 1);
+            if (!cur.contains(i)) {
+                cur.add(i);
+                bruteForce(start + 1, k, map, emptyList, cur);
+                cur.remove(cur.size() - 1);
+            }
+
+        }
+    }
+
+    public static class Point{
+        int row;
+        int col;
+
+        public Point(int row, int col){
+            this.row = row;
+            this.col = col;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return row == point.row && col == point.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "row=" + row +
+                    ", col=" + col +
+                    '}';
+        }
     }
 }
