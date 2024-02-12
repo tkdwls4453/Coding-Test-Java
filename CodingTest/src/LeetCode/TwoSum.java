3e8b4194faf786bb3b51bf1981b098a89a386f9a
Yoon321 <tkdwls734@gmail.com>
2024-02-12 22:24:45 +0900

    [LeetCode] TwoSum
    HashMap을 이용해 성능 최적화 시킴

--- CodingTest/src/LeetCode/TwoSum.java
+++ CodingTest/src/LeetCode/TwoSum.java
@@ -14,6 +14,7 @@ public class TwoSum {
         System.out.println(Arrays.toString(twoSum1(nums, target)));
         System.out.println(Arrays.toString(twoSum2(nums, target)));
         System.out.println(Arrays.toString(twoSum3(nums, target)));
+        System.out.println(Arrays.toString(twoSum4(nums, target)));
     }
 
     /**
@@ -88,4 +89,21 @@ public class TwoSum {
     }
 
 
+    /**
+     * HashMap 이용한 성능 최적화
+     */
+    public static int[] twoSum4(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+            if(map.containsKey(needed)){
+                return new int[]{map.get(needed), i};
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
 }
