539bbcfd57b37d8a2b186f4349bd9a6ad43f28a2
Yoon321 <tkdwls734@gmail.com>
2024-02-13 22:59:33 +0900

    [Backjoon] 연구소 - 다시풀기
    https://www.acmicpc.net/problem/14502

--- CodingTest/src/Backjoon/Laboratory.java
+++ CodingTest/src/Backjoon/Laboratory.java
@@ -7,123 +7,119 @@ import java.util.*;
  * https://www.acmicpc.net/problem/14502
  */
 public class Laboratory {
-
-    static final int[][] moves = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static int maxEmptyArea = 0;
+    final static int WALL_CNT = 3;
+    final static int[][] moves = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static List<List<Integer>> wallList = new ArrayList<>();
+    static List<Vertex> virusList = new ArrayList<>();
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
         int[][] map = new int[n][m];
-        List<int[]> emptyList = new ArrayList<>();
+        int maxEmptySpace = 0;
+        List<int[]> emptySpaces = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                map[i][j] = sc.nextInt();
+                map[i][j] = scanner.nextInt();
+
                 if (map[i][j] == 0) {
-                    emptyList.add(new int[]{i, j});
+                    emptySpaces.add(new int[]{i, j});
+                }else if(map[i][j] == 2){
+                    virusList.add(new Vertex(i, j));
                 }
             }
         }
 
-        bruteForce(0,3, map, emptyList, new ArrayList<>());
+        backtracking(0, emptySpaces, new ArrayList<>());
 
-        System.out.println(maxEmptyArea);
+        for (List<Integer> walls : wallList) {
+            int[][] newMap = createNewMap(map, walls, emptySpaces);
 
-    }
+            // BFS 코드
+            Queue<Vertex> queue = new LinkedList<>();
 
-    public static void bruteForce(int start, int k, int[][] map, List<int[]> emptyList, List<Integer> cur){
-        int length = emptyList.size();
-        if (cur.size() == k) {
-            int curEmptyArea = 0;
-            int count = 0;
-            Map<Point, Boolean> wallMap = new HashMap<>();
-            for (int idx : cur) {
-                int[] wall = emptyList.get(idx);
-                wallMap.put(new Point(wall[0], wall[1]), true);
+            for (Vertex virus : virusList) {
+                queue.offer(virus);
+                newMap[virus.r][virus.c] = 2;
             }
 
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[0].length; j++) {
-                    if (map[i][j] == 1) {
-                        wallMap.put(new Point(i, j), true);
-                    }
-                }
-            }
-            // bfs 코드
-            Queue<Point> queue = new LinkedList<>();
-            Map<Point, Boolean> visited = new HashMap<>();
+            while (!queue.isEmpty()) {
+                Vertex cur = queue.poll();
 
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[0].length; j++) {
-                    if (map[i][j] == 2) {
-                        queue.offer(new Point(i, j));
-                        visited.put(new Point(i, j), true);
-                        count++;
-                    }
-                }
-            }
-
-            while(!queue.isEmpty()){
-                Point now = queue.poll();
                 for (int[] move : moves) {
-                    int nextR = now.row + move[0];
-                    int nextC = now.col + move[1];
-                    if (nextR >= 0 && nextR < map.length && nextC >= 0 && nextC < map[0].length) {
+                    int nextR = cur.r + move[0];
+                    int nextC = cur.c + move[1];
 
-                        if (!visited.containsKey(new Point(nextR, nextC)) && !wallMap.containsKey(new Point(nextR, nextC))) {
-                            queue.offer(new Point(nextR, nextC));
-                            visited.put(new Point(nextR, nextC), true);
-                            count++;
+                    if (nextR >= 0 && nextR < map.length && nextC >= 0 && nextC < map[0].length) {
+                        Vertex nextVertex = new Vertex(nextR, nextC);
+                        if (newMap[nextR][nextC] == 0) {
+                            queue.offer(nextVertex);
+                            newMap[nextR][nextC] = 2;
                         }
                     }
                 }
             }
-            curEmptyArea = map.length * map[0].length - count - wallMap.size();
-            maxEmptyArea = Math.max(curEmptyArea, maxEmptyArea);
-            return;
+
+            int emptySpaceCnt = countEmptySpace(newMap);
+            maxEmptySpace = Math.max(maxEmptySpace, emptySpaceCnt);
         }
 
-        for (int i = start; i < length; i++) {
-            if (!cur.contains(i)) {
-                cur.add(i);
-                bruteForce(start + 1, k, map, emptyList, cur);
-                cur.remove(cur.size() - 1);
-            }
+        System.out.println(maxEmptySpace);
+    }
 
+    static class Vertex{
+        private int r;
+        private int c;
+
+        public Vertex(int r, int c){
+            this.r = r;
+            this.c = c;
         }
     }
 
-    public static class Point{
-        int row;
-        int col;
+    static int countEmptySpace(int[][] map){
+        int count = 0;
 
-        public Point(int row, int col){
-            this.row = row;
-            this.col = col;
+        for (int i=0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] == 0) {
+                    count++;
+                }
+            }
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return row == point.row && col == point.col;
+        return count;
+    }
+
+    static int[][] createNewMap(int[][] map, List<Integer> walls, List<int[]> emptySpaces) {
+        int[][] newMap = new int[map.length][map[0].length];
+
+        for (int i=0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                newMap[i][j] = map[i][j];
+            }
         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
+        for (int wallIdx : walls) {
+            int[] wall = emptySpaces.get(wallIdx);
+            newMap[wall[0]][wall[1]] = 1;
         }
 
-        @Override
-        public String toString() {
-            return "Point{" +
-                    "row=" + row +
-                    ", col=" + col +
-                    '}';
+        return newMap;
+    }
+    static void backtracking(int start, List<int[]> emptySpaces, List<Integer> cur){
+        if (cur.size() == WALL_CNT) {
+            wallList.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int i = start; i < emptySpaces.size(); i++) {
+            if (!cur.contains(i)) {
+                cur.add(i);
+                backtracking(i + 1, emptySpaces, cur);
+                cur.remove(cur.size() - 1);
+            }
         }
     }
 }
