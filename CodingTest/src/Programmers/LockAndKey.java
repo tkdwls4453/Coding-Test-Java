af63a752c839b376280e974bf16e0c26a687efbb
Yoon321 <tkdwls734@gmail.com>
2024-02-14 19:50:18 +0900

    [Programmers] 자물쇠와 열쇠 - 다시풀기
    link : https://school.programmers.co.kr/learn/courses/30/lessons/60059

--- CodingTest/src/Programmers/LockAndKey.java
+++ CodingTest/src/Programmers/LockAndKey.java
@@ -11,23 +11,31 @@ public class LockAndKey {
         System.out.println(solution(key, lock));
     }
     public static boolean solution(int[][] key, int[][] lock) {
+
+        // 자물쇠의 기준
+        // 확장된 자물쇠에서 기존의 자물쇠 위치를 찾기 위해 필요
         int offset = key.length - 1;
 
         for (int r = 0; r < key.length + lock.length - 1; r++) {
             for (int c = 0; c < key.length + lock.length - 1; c++) {
+                // 이 반복문을 통해 열쇠의 기준을 이동 시킨다.
+
                 for (int rotate = 0; rotate < 4; rotate++) {
-                    // 4번 회전
+                    // 4번 회전시키기 위한 반복문
+
+                    // 확장된 자물쇠를 만든다.
                     int newLockSize = key.length * 2 + lock.length - 2;
                     int[][] newLock = new int[newLockSize][newLockSize];
-
                     for (int i = 0; i < lock.length; i++) {
                         for (int j = 0; j < lock.length; j++) {
                             newLock[i + offset][j + offset] = lock[i][j];
                         }
                     }
 
-                    // newLock 에 해당 key 를 더함
+                    // newLock 에 해당 key 를 더한다.
                     match(newLock, key, r, c, rotate);
+
+                    // 해당 키가 자물쇠를 열 수 있는지 확인한다.
                     if(check(newLock, offset, lock.length)){
                         return true;
                     }
