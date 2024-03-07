8c0c379d9a7c45571969173f29dc002f96667ec8
Yoon321 <tkdwls734@gmail.com>
2024-03-07 22:03:27 +0900

    [Programmers] 두 큐합 같게 만들기
    link : https://school.programmers.co.kr/learn/courses/30/lessons/118667

--- CodingTest/src/Programmers/MakeSumOfTwoQueueEqual.java
+++ CodingTest/src/Programmers/MakeSumOfTwoQueueEqual.java
@@ -1,6 +1,7 @@
 package Programmers;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 두 큐합 같게 만들기
@@ -8,45 +9,47 @@ import java.util.*;
  */
 public class MakeSumOfTwoQueueEqual {
     public static void main(String[] args){
-        int[] queue1 = {9, 7, 2};
-        int[] queue2 = {9, 2, 11};
+        int[] queue1 = {1, 1};
+        int[] queue2 = {1, 5};
 
         System.out.println(solution(queue1, queue2));
     }
 
     public static int solution(int[] queue1, int[] queue2) {
-        int answer = -1;
-        Queue<Long> q1 = createQueue(queue1);
-        Queue<Long> q2 = createQueue(queue2);
-        int size = queue1.length * 4;
-        Long q1Sum = q1.stream().mapToLong(l -> l).sum();
-        Long q2Sum = q2.stream().mapToLong(l -> l).sum();
+        int result = 0;
+        Queue<Integer> queueOne = Arrays.stream(queue1)
+                .boxed()
+                .collect(Collectors.toCollection(LinkedList::new));
 
+        Queue<Integer> queueTwo = Arrays.stream(queue2)
+                .boxed()
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        long sumOne = queueOne.stream().mapToLong(Integer::longValue).sum();
+        long sumTwo = queueTwo.stream().mapToLong(Integer::longValue).sum();
+
+        int size = queue1.length * 4;
         for (int i = 0; i < size; i++) {
-            if (q1Sum < q2Sum) {
-                Long num = q2.poll();
-                q1.offer(num);
-                q2Sum -= num;
-                q1Sum += num;
-            }else if(q1Sum > q2Sum){
-                Long num = q1.poll();
-                q2.offer(num);
-                q1Sum -= num;
-                q2Sum += num;
+            if (sumOne == sumTwo) {
+                return result;
+            }
+
+            if (sumOne < sumTwo) {
+                int num = queueTwo.poll();
+                queueOne.offer(num);
+                sumTwo -= num;
+                sumOne += num;
+                result++;
             }else{
-                return i;
+                int num = queueOne.poll();
+                queueTwo.offer(num);
+                sumOne -= num;
+                sumTwo += num;
+                result++;
             }
         }
-        return answer;
-    }
 
-    public static Queue<Long> createQueue(int[] arr){
-        Queue<Long> queue = new LinkedList<>();
-
-        for(int num : arr){
-            queue.offer((long) num);
-        }
-        return queue;
+        return -1;
     }
 
 }
