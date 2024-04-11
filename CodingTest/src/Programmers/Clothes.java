a5bf8e1805c55d22902372e06b321062f21d2ac8
Yoon321 <tkdwls734@gmail.com>
2024-04-11 17:44:39 +0900

    [Programmers] 의상
    https://school.programmers.co.kr/learn/courses/30/lessons/42578

--- CodingTest/src/Programmers/Clothes.java
+++ CodingTest/src/Programmers/Clothes.java
@@ -6,32 +6,19 @@ import java.util.Map;
 import java.util.Set;
 
 public class Clothes {
-    static int answer = 0;
     public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> clothesCnt = new HashMap<>();
 
-        Map<String, Boolean> visited = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        for(String[] clothe : clothes){
-            set.add(clothe[1]);
-        }
-        int max = set.size();
-        dfs(0, clothes, visited, max);
-        return answer;
-    }
-
-    public void dfs(int start, String[][] clothes, Map<String, Boolean> visited, int max){
-        if(visited.size() >= max){
-            return;
+        for(String[] arr : clothes){
+            clothesCnt.putIfAbsent(arr[1], 0);
+            clothesCnt.put(arr[1], clothesCnt.get(arr[1]) + 1);
         }
 
-        for(int i=start; i<clothes.length; i++){
-            if(!visited.containsKey(clothes[i][1])){
-                answer++;
-                visited.put(clothes[i][1], true);
-                dfs(i+1, clothes, visited, max);
-                visited.remove(clothes[i][1]);
-            }
+        for(String key : clothesCnt.keySet()){
+            answer *= (clothesCnt.get(key) + 1);
         }
+        return answer - 1;
     }
 }
 
